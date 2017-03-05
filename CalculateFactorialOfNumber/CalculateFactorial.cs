ba55fd8911fd5380737bd6c3e118ca36205155d5
cshah <cshah@tavisca.com>
2017-03-05 23:09:41 +0530

    Initial commit

---
+++ CalculateFactorialOfNumber/CalculateFactorial.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateFactorialOfNumber
+{
+    public class CalculateFactorial
+    {
+       public int FactorialFunction(int factorialNumber)
+        {
+            int factorialResult = 1;
+            for(int iterator = factorialNumber ; iterator >= 1;iterator--)
+            {
+                factorialResult *= iterator;
+            }
+            return factorialResult;
+        }
+    }
+}
---
+++ CalculateFactorialOfNumber/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateFactorialOfNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userInputNumber;
+            
+            Console.WriteLine("Enter The Number");
+            userInputNumber = Convert.ToInt32(Console.ReadLine());
+            CalculateFactorial calculateFactorialObject = new CalculateFactorial();
+            Console.WriteLine("Factorial of {0} is : {1}",userInputNumber,calculateFactorialObject.FactorialFunction(userInputNumber));
+            Console.WriteLine("Thankyou\nPress Any key to Exit the Application...");
+            Console.ReadLine();
+        }
+    }
+}
